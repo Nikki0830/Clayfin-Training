e1f3ae21bc1ac07c4e58c4b0926dc1fa2aff1b44
Nikita Patel <69712671+Nikki0830@users.noreply.github.com>
2022-06-09 14:42:53 +0530

    Add files via upload

---
+++ CLAYFIN TRAINING/arrowfun.js
@@ -0,0 +1,13 @@
+//example
+
+const numbers = function (x, y) {
+  return x + y;
+};
+console.log(numbers(1, 2));
+
+const number = (x, y) => x + y;
+console.log(number(1, 2));
+
+//output
+//3
+//3
