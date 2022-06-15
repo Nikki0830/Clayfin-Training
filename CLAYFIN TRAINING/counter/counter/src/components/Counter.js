32c65cc2dd9b08a72f929d46620d5735e3cab994
Nikita Patel <69712671+Nikki0830@users.noreply.github.com>
2022-06-15 10:20:06 +0530

    Add files via upload

---
+++ CLAYFIN TRAINING/counter/counter/src/components/Counter.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import "./Counter.css";
+export class Counter extends Component {
+  state = { Counter: 0 };
+  handleInc = () => {
+    this.setState({
+      Counter: this.state.Counter + 1,
+    });
+  };
+  handleDec = () => {
+    this.setState({
+      Counter: this.state.Counter - 1,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <div
+          className="container"
+          style={{
+            backgroundColor: "blue",
+            width: "100px",
+            height: "100px",
+            borderRadius:"50%",
+            backgroundColor:
+              this.state.Counter == 0
+                ? "green"
+                : this.state.Counter > 0
+                ? "yellow"
+                : "red",
+          }}
+        ></div>
+        <h1 className="h1">{this.state.Counter}</h1>
+        <button className="button1" onClick={this.handleInc}>+</button>
+        <button className="button2" onClick={this.handleDec}>-</button>
+      </>
+    );
+  }
+}
+
+export default Counter;
