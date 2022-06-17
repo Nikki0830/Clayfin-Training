4387f692ef223a33d44bba05b92c7d0a6690d612
Davis Kumar Nayak <you@example.com>
2022-06-17 11:13:56 +0530

    16th june

---
+++ CLAYFIN TRAINING/16th june/my-app/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import Gallery2 from "./components/Gallery/Gallery2"
+
+function App() {
+  return (
+    <>
+    <div className="App">
+     <Gallery2/>
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ CLAYFIN TRAINING/16th june/my-app/src/components/Gallery/Gallery.js
@@ -0,0 +1,68 @@
+export const data = [
+  {
+    name: "Flower",
+    description: "This is a flower",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKxZgVYzUaGDY_4vxzSp_QQYPKZQjZySJZ6Q&usqp=CAU",
+  },
+  {
+    name: "Animal",
+    description: "This is Animal",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9OVk4yeHj7j9yi6HFMxr6GAhHYzUvZIM4Xg&usqp=CAU",
+  },
+  {
+    name: "Fruits",
+    description: "These are fruit",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnTS8LGXByZVkKnw9xqtmVcTGuzAOD6DNJPw&usqp=CAU",
+  },
+  {
+    name: "Laptop",
+    description: "This is a Laptop",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqpwlPxJiSdJMLTit3RLj78lHk8WDaJ-aM2w&usqp=CAU",
+
+  },
+  {
+    name: "Light",
+    description: "This is a Light",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDjcA6iVzoB0-FZMuYvoF6Uo2anzWBCreKTw&usqp=CAU",
+
+  },
+
+  {
+    name: "Flower",
+    description: "This is a flower",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKxZgVYzUaGDY_4vxzSp_QQYPKZQjZySJZ6Q&usqp=CAU",
+  },
+  {
+    name: "Animal",
+    description: "This is Animal",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9OVk4yeHj7j9yi6HFMxr6GAhHYzUvZIM4Xg&usqp=CAU",
+  },
+  {
+    name: "Fruits",
+    description: "These are fruit",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnTS8LGXByZVkKnw9xqtmVcTGuzAOD6DNJPw&usqp=CAU",
+  },
+  {
+    name: "Laptop",
+    description: "This is a Laptop",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqpwlPxJiSdJMLTit3RLj78lHk8WDaJ-aM2w&usqp=CAU",
+
+  },
+  {
+    name: "Light",
+    description: "This is a Light",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDjcA6iVzoB0-FZMuYvoF6Uo2anzWBCreKTw&usqp=CAU",
+
+  },
+
+];
+
+
